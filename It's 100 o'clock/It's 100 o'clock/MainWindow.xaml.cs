8a44e2a0fc2d14d3c3ea2726cd0d8dc98f361aa7
Anzlc <107500985+Anzlc@users.noreply.github.com>
2022-08-12 20:10:25 +0200

    Update Loop
    Added update loop.

--- It's 100 o'clock/It's 100 o'clock/MainWindow.xaml.cs
+++ It's 100 o'clock/It's 100 o'clock/MainWindow.xaml.cs
@@ -1,28 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Collections;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace It_s_100_o_clock
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+
     public partial class MainWindow : Window
     {
+        public readonly float ConvertTo100 = 11.57407407407407f;
+        internal DateTime dt = DateTime.Now;
+        internal DispatcherTimer timer = new DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
+            ClockLabel.Content = dt.Second;
+            
+            
         }
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 500);
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+        }
+    
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            dt = DateTime.Now;
+            ClockLabel.Content = dt.Second;
+        }
+
+
     }
 }
